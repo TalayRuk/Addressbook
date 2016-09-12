4d84d144a379bfc1ffe930039d7ccec433d06822
Talayruk <notejde@gmail.com>
2016-09-12 03:10:31 -0700

    add objects.cs methods

--- Objects/contacts.cs
+++ Objects/contacts.cs
@@ -18,8 +18,44 @@ namespace Addressbook.Objects
             _phoneNumber = PhoneNumber;
             _address = Address;
             _contacts = new List<Contact>{};
+            _instances.Add(this);
+        }
 
-
+        public string GetName()
+        {
+            return _name;
+        }
+        public void SetName(string newName)
+        {
+            _name = newName;
+        }
+        public int GetPhoneNumber()
+        {
+            return _phoneNumber;
+        }
+        public void SetPhoneNumber(int newPhoneNumber)
+        {
+            _phoneNumber = newPhoneNumber;
+        }
+        public string GetAddress()
+        {
+            return _address;
+        }
+        public void SetAddress(string newAddress)
+        {
+            _address = newAddress;
+        }
+        public static List<Contact> GetAll()
+        {
+            return _instances;
+        }
+        public void Save(Contact newContact)
+        {
+            _instances.Add(newContact);
+        }
+        public static void DeleteAll()
+        {
+            _instances.Clear();
         }
     }
 }
